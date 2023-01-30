8bd12ae25c6e5fe0522cc566c13935972d95a2b9
Jain <janepark423@gmail.com>
2023-01-31 04:21:49 +0900

    랜덤 주사위

--- app/src/main/java/com/example/dice_app/MainActivity.kt
+++ app/src/main/java/com/example/dice_app/MainActivity.kt
@@ -19,12 +19,36 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val diceImage1 = binding.dice1
+        val diceImage2 = binding.dice2
+
         binding.diceStartBtn.setOnClickListener {
 
             Toast.makeText(this, "주사위 go", Toast.LENGTH_LONG).show()
 
             Log.d("mainactivity", Random.nextInt(1,6).toString())
             Log.d("mainactivity", Random.nextInt(1,6).toString())
+
+            val number1= Random.nextInt(1,6)
+            val number2= Random.nextInt(1,6)
+
+            if(number1==1){
+                diceImage1.setImageResource(R.drawable.dice_1)
+            }else if(number1==2){
+                diceImage1.setImageResource(R.drawable.dice_2)
+            }else if(number1==3) {
+                diceImage1.setImageResource(R.drawable.dice_3)
+            }else if(number1==4) {
+                diceImage1.setImageResource(R.drawable.dice_4)
+            }else if(number1==5) {
+                diceImage1.setImageResource(R.drawable.dice_5)
+            }else if(number1==6) {
+                diceImage1.setImageResource(R.drawable.dice_6)
+            }
+
+
+            diceImage2.setImageResource(R.drawable.dice_2)
+
            // Random.nextInt(1,6)
 
         }
